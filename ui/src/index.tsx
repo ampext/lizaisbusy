661e998e1aa775eb19eef9dcab538d4c72f28dc9
ampext <cowsayhelloworld@gmail.com>
2019-09-01 21:33:26 +0200

    added css variables for colors

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import './variables.css';
 
 ReactDOM.render(<App />, document.querySelector('#app'));
